7293f506333fa505064262e7d0cede18c749772b
saarmartial <saramt200411@gmail.com>
2025-05-26 19:27:37 +0200

    Add weekly planner function with random deed and duration generation

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+import random
+
 print("welcome to python week planer")
+
+week_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+deeds_array = ["Math", "Checkio", "Persian", "Reading", "Sport", "Philosophy"]
+
+duration_array = ["45min", "90min", "1h", "2h"]
+
+def week_plan() -> str:
+    for i in range(7):
+        print("Deeds of " + week_days[i] + ": ")
+        for n in range(3):
+            print(str(n + 1) + ". " + random.choice(deeds_array))
+            print("duration: " + random.choice(duration_array))
+    
+    return "This is your weekplan"
+
+print(week_plan())
+
